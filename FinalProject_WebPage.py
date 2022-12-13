93bcab35bcf084bceee069b9be79cfa14ff0771d
Vishnu Saju <52094332+Vishnusaju24@users.noreply.github.com>
2022-12-13 03:00:47 -0500

    Update FinalProject_WebPage.py

--- FinalProject_WebPage.py
+++ FinalProject_WebPage.py
@@ -12,26 +12,21 @@ import seaborn as sns
 df=pd.read_csv('Metadata.csv')
 st.title('Flight Ticket Price Prediction')
 st.write('Hello, *World!* :sunglasses: Welcome to Our Final Project WebPage....')
-st.write('The page shows predicted flight ticket prices on a given day based on state of the art model devolpment done on 60 days of data from the day')
+st.write('The page shows expected flight ticket prices in the next 60 days')
 st.write('Happy Landings!! - Vishnu & Christy')
-st.subheader('Dataset')
+st.subheader('Data Trend')
 st.dataframe(df)
 st.subheader('Data Numerical Statistic')
 st.dataframe(df.describe())
-st.subheader('Data Visualization with respect to Survived')
+st.subheader('Data Visualizations')
 left_column, right_column = st.columns(2)
 with left_column:
    #'Numerical Plot'
     num_feat = st.selectbox(
    'Select Numerical Feature', df.select_dtypes('number').columns)
-    fig = px.line(df, x="Date", y=num_feat, title='Flight Cost Trend',color="Date")
-
-
-    st.plotly_chart(fig, use_container_width=True)
+    fig = px.scatter(df, x="Date", y=num_feat, title='Flight Cost Trend',color="Date")
+st.plotly_chart(fig, use_container_width=True)
 with right_column:
    #'Categorical column'
-    cat_feat = st.selectbox(
-    'Select Categorical Feature', df.select_dtypes(exclude =   'number').columns)
-    fig = fig = px.line(df, x="Date", y="Average Flight Cost", title='Average Flight Cost')
+    fig =px.bar(df, x="Date", y=num_feat, title='Flight Cost Trend',color="Date")
 st.plotly_chart(fig, use_container_width=True)
-
